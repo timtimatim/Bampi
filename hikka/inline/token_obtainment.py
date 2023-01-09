9e2430e6781360042d61d552d4b9ef964abeb557
timtimatim <108235289+timtimatim@users.noreply.github.com>
2023-01-09 09:07:22 +0200

    Починил баг Бороса

--- hikka/inline/token_obtainment.py
+++ hikka/inline/token_obtainment.py
@@ -157,7 +157,7 @@ class TokenObtainment(InlineUnit):
                         "hikka.inline",
                         "custom_bot",
                         False,
-                    ) and not re.search(r"@hikka_[0-9a-zA-Z]{6}_bot", button.text):
+                    ) and not re.search(r"@bampi_[0-9a-zA-Z]{6}_bot", button.text):
                         continue
 
                     m = await conv.send_message(button.text)
